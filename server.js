e87a16deb94cde8a65b7f4e770b894fef6a9ae19
Shahid Hussain <shahidhussain@Shahids-Air.broadband>
2023-11-13 01:04:36 +0000

    Preapering for production

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const path = require("path");
+
 const app = express();
 const apiInfoRouter = require("./routes/apiInfoRotes");
 const authRouter = require("./routes/authRoutes");
@@ -9,7 +11,10 @@ const adRouter = require("./routes/adRoutes");
 app.use(express.json({ limit: "10mb" }));
 app.use(morgan("dev"));
 app.use(cors());
-
+app.use(express.static(path.join(__dirname, ".", "public")));
+app.get("/*", function (req, res) {
+	res.sendFile(path.join(__dirname, "public"));
+});
 app.use("/api/v1/info", apiInfoRouter);
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/ad", adRouter);
